1368d44d320e438f6f2a7c485a3b5a8edda15719
AbbosxonDeveloper <abbosxonoripov3@gmail.com>
2024-01-13 10:35:16 +0500

    format js file

--- script.js
+++ script.js
@@ -8,52 +8,52 @@ let plusminus = document.getElementById("plusminus");
 let btn_stop = document.getElementById("trade_btn_stop");
 let trade_btn_get = document.getElementById("trade_btn_get");
 
-let getCash = JSON.parse(localStorage.getItem("cash"))
+let getCash = JSON.parse(localStorage.getItem("cash"));
 if (!getCash) {
-    getCash = 100
+    getCash = 100;
 }
-let storageCash = localStorage.setItem("cash", getCash)
+let storageCash = localStorage.setItem("cash", getCash);
 
-console.log(+getCash)
-cash.textContent = Number(getCash).toFixed(2)
+// console.log(+getCash)
+cash.textContent = Number(getCash).toFixed(2);
 
 // line.textContent = randline;
 trade_form.onsubmit = (e) => {
     e.preventDefault();
     if (trade_input.value > getCash) {
-        warn.textContent = "you don't have enough money"
+        warn.textContent = "you don't have enough money";
         setTimeout(() => {
-            warn.textContent = null
+            warn.textContent = null;
         }, 2500)
     } else if (trade_input.value == 0) {
-        warn.textContent = "you cannot trade with 0"
+        warn.textContent = "you cannot trade with 0";
         setTimeout(() => {
-            warn.textContent = null
+            warn.textContent = null;
         }, 2500)
     } else {
         line.textContent = trade_input.value;
-        localStorage.setItem("cash", JSON.stringify(+getCash - (+trade_input.value)))
-        cash.textContent = (+getCash - +trade_input.value).toFixed(2)
+        localStorage.setItem("cash", JSON.stringify(+getCash - (+trade_input.value)));
+        cash.textContent = (+getCash - +trade_input.value).toFixed(2);
 
         trade_btn.style.display = "none";
         btn_stop.style.display = "block";
         let interval = setInterval(function () {
-            console.log('nimadir');
+            // console.log('nimadir');
             let randline = Math.floor(Math.random() * 10000);
             let rand = Number((Math.random() * 10).toFixed(2));
             if (randline % 2 !== 0) {
-                line.textContent = parseFloat((+line.textContent - +line.textContent * rand / 100).toFixed(2))
-                plusminus.style.color = "red"
+                line.textContent = parseFloat((+line.textContent - line.textContent * rand / 100).toFixed(2));
+                plusminus.style.color = "red";
                 plusminus.textContent = '-' + +(rand).toFixed(2) + "%";
             } else {
-                line.textContent = parseFloat((+line.textContent + +line.textContent * rand / 100).toFixed(2))
-                plusminus.style.color = "green" 
+                line.textContent = parseFloat((+line.textContent + line.textContent * rand / 100).toFixed(2));
+                plusminus.style.color = "green";
                 plusminus.textContent = '+' + +(rand).toFixed(2) + "%";
             }
         }, 1000)
 
         btn_stop.onclick = () => {
-            btn_stop.style.display = "none"
+            btn_stop.style.display = "none";
             clearInterval(interval, 0);
             trade_btn_get.style.display = "block";
 
@@ -61,9 +61,8 @@ trade_form.onsubmit = (e) => {
                 // 
                 localStorage.setItem("cash", JSON.stringify(+cash.textContent + +line.textContent));
                 // console.log(+line.textContent);
-                location.reload()
+                location.reload();
             }
         }
-
     }
 }
